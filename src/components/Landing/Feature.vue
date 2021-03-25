f721579d8c2d914b9dd44771e1abcf249356b416
ErenKaya1 <wtcerberus@gmail.com>
2021-03-25 20:46:59 +0300

    Feature component designed

---
+++ src/components/Landing/Feature.vue
@@ -0,0 +1,18 @@
+<template>
+  <b-col md="4" class="mt-5">
+    <div class="display-4 text-landing-primary mx-auto mb-4">
+      <span :class="iconClass"></span>
+    </div>
+    <h5>{{ title }}</h5>
+    <div class="custom-text-muted small">{{ description }}</div>
+  </b-col>
+</template>
+
+<script>
+export default {
+  props: ["title","description","iconClass"]
+};
+</script>
+
+<style>
+</style>
