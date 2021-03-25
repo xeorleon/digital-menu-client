c960758fb30c733b0712750143e57bb0b8465df5
ErenKaya1 <wtcerberus@gmail.com>
2021-03-25 05:33:04 +0300

    PageLoader component

---
+++ src/components/PageLoader.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="page-loader">
+    <div class="bg-landing-primary"></div>
+  </div>
+</template>
+
+<script>
+import Pace from "pace-js";
+
+export default {
+  mounted() {
+    Pace.start();
+  },
+};
+</script>
+
+<style>
+.pace .pace-progress {
+  background-color: var(--color-landing-primary) !important;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import BootstrapVue from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import "pace-js/pace-theme-default.min.css";
+import "./variables.css";
+import "./styles.css";
 
 Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
 
 new Vue({
   router,
