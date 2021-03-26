d8f64b4e1a7d80e62ea808afc2eb84d1b8cfee54
ErenKaya1 <wtcerberus@gmail.com>
2021-03-26 04:30:38 +0300

    Pricing component design has been updated

--- src/components/Landing/Pricing.vue
+++ src/components/Landing/Pricing.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <b-row no-gutters class="row-bordered text-center pt-4">
-      <b-col md="4" class="d-flex flex-column py-4" v-for="(type, index) in subscriptionTypes" :key="index">
+    <b-row no-gutters class="text-center pt-4">
+      <b-col md="4" class="d-flex flex-column py-4" :class="index != 2 ? 'pricing-bordered' : ''" v-for="(type, index) in subscriptionTypes" :key="index">
         <h5 class="m-0">{{ type.title }}</h5>
         <div class="flex-grow-1">
           <div class="py-4 my-2">
             <span class="d-inline-block text-muted text-big align-middle mr-2">₺</span>
-            <span class="display-3 d-inline-block font-weight-bold align-middle">{{ type.price }}</span>
+            <span class="display-3 d-inline-block font-weight-bold align-middle pricing-price">{{ type.price }}</span>
             <span class="d-inline-block text-muted text-big align-middle ml-2">/ mo</span>
           </div>
           <div class="pb-5">
@@ -29,3 +29,19 @@ export default {
   props: ["subscriptionTypes"],
 };
 </script>
+
+<style scoped>
+.pricing-bordered {
+  border-right: 1px solid var(--color-muted);
+}
+
+h5 {
+  font-weight: 500;
+  color: var(--color-footer-text);
+}
+
+.pricing-price {
+  font-weight: 700;
+  color: var(--color-footer-text);
+}
+</style>
