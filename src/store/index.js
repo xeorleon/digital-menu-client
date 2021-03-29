2de2f767bd7c05502c1c55276e0a93718ad2531c
xeorleon <xeorleon@gmail.com>
2021-03-29 19:37:27 +0300

    Configured store

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,29 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  state: {
+    token: null,
+    user: null,
+    isUserLoggedIn: false,
+  },
+  mutations: {
+    setToken(state, token) {
+      if (token) {
+        state.isUserLoggedIn = true;
+      } else {
+        state.isUserLoggedIn = false;
+      }
+    },
+    setUser(state, user) {
+      state.user = user;
+    },
+  },
+  actions: {
+    setToken({commit},token){
+      commit('setToken',token)
+    },
+    setUser({commit},user){
+      commit('setUser',user)
+    }
+  },
 });
