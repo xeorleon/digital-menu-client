83915d9cb4840dba05223a034342291798e9090b
ErenKaya1 <wtcerberus@gmail.com>
2021-04-05 01:35:05 +0300

    created categories view

--- src/views/Dashboard/Categories.vue
+++ src/views/Dashboard/Categories.vue
@@ -1,11 +1,27 @@
 <template>
-  <div>categories</div>
+  <div>
+    <b-row>
+      <b-col cols="12">
+        <b-button v-b-modal.new-category variant="landing-secondary">Yeni Kategori</b-button>
+      </b-col>
+      <new-category-modal />
+    </b-row>
+    <b-row class="mt-4">
+      <b-col cols="12">
+        <categories-table />
+      </b-col>
+    </b-row>
+  </div>
 </template>
 
 <script>
+import CategoriesTable from "@/components/Dashboard/Categories/CategoriesTable.vue";
+import NewCategoryModal from "../../components/Dashboard/Categories/NewCategoryModal.vue";
 export default {
+  components: { CategoriesTable, NewCategoryModal },
+
   mounted() {
-    this.$title = "Categories";
+    this.NewCategoryModal$title = "Categories";
   },
 };
 </script>
