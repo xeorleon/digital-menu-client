e9b682a813eafb2a077e55dbbc9fdedb1d27eed5
ErenKaya1 <wtcerberus@gmail.com>
2021-04-18 05:53:00 +0300

    added navbar component for menu view

---
+++ src/components/Menu/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <b-navbar>
+    <b-container>
+      <b-navbar-brand to="/" class="text-big font-weight-bolder line-height-1 text-expanded py-3">
+        <b-img v-if="this.logo" :src="this.logo" class="menu-logo" fluid />
+      </b-navbar-brand>
+    </b-container>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  props: ["logo"],
+};
+</script>
+
+<style>
+.menu-logo {
+  height: 60px !important;
+}
+</style>
